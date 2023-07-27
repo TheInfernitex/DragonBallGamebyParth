3c44970256106277f1c056017c44fdf3be5d2024
Parth Agarwal <110246608+TheInfernitex@users.noreply.github.com>
2023-07-27 19:18:39 +0530

    Update work.js

--- work.js
+++ work.js
@@ -38,6 +38,11 @@ start.addEventListener("click",()=>{
     // check=setInterval(working,10);
     rel.style.visibility="hidden";
     fight.innerText="FIGHT!";
+     
+    hero.style.bottom="60px";
+    hero.style.left="200px";
+    villain.style.bottom="60px";
+    villain.style.left="calc(100% - 370px)";
     
     heroHealth=100;villainHealth=100;
     audio.play();
@@ -52,10 +57,6 @@ rel.addEventListener("click",function(){
     per2.innerText=villainHealth+"%";
     fight.innerText="FIGHT!";
     // audio.play();
-    hero.style.bottom="60px";
-    hero.style.left="200px";
-    villain.style.bottom="60px";
-    villain.style.left="calc(100% - 370px)";
     
     hero.style.bottom="60px";
     hero.style.left="200px";
